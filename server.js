63a7349c6e03164af8ea72c298e5b659e9c96402
Amin Meyghani <meyghania@gmail.com>
2016-10-30 16:17:42 -0700

    enable logging.

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
+const FileStreamRotator = require('file-stream-rotator');
 const express = require('express');
+const fs = require('fs');
+const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
+const logDirectory = path.join('/var/log', 'node-app');
+
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+// create a rotating write stream
+const accessLogStream = FileStreamRotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: path.join(logDirectory, 'access-%DATE%.log'),
+  frequency: 'daily',
+  verbose: false
+});
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
+
 
 app.get('/api/data', (req, res) => {
   res.json({
